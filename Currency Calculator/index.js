a00ed8cd193e5a3b148a7f4590feaa4bd2eb7259
SplashCode01 <43210754+SplashCode01@users.noreply.github.com>
2021-06-25 20:03:50 +0400

    Update on JSON and AJAX

--- Currency Calculator/index.js
+++ Currency Calculator/index.js
@@ -1,3 +1,46 @@
+var array = {};
+var rate;
+var selectedCurrency;
+var selectedDate;
+$(document).ready(function () {
+
+    $('.currency').click(function () {  
+        $('.currencies-list').empty();   
+         $.ajax({
+             type: "GET",
+             url: "Currencies.json",
+             dataType: "json",
+             success: function(data) {
+                 $.each(data, function (i, option) {
+                    populateCurrencies(option.name, option.code, '.currencies-list', "currencies dropdown-item");
+                 })
+             },
+             error: function(){
+                 alert("json not found");
+             }
+         });
+     });
+    
+ 
+     $('.rate').click(function () {
+         $('.rates-list').empty();
+         $.ajax({
+             type: "GET",
+             url: "Rates.json",
+             dataType: "json",
+             success: function (data) {
+                 array = data.GEL;
+                 $.each(data.GEL, function (i, option) {
+                    populateCurrencies(option.date, option.date, '.rates-list', "rates dropdown-item");
+                 })
+             },
+             error: function () {
+                 alert("json not found");
+             }
+         });
+     });
+});
+
 function disableOperators(){
     var elems = document.getElementsByClassName("operator");
     for(var i = 0; i < elems.length; i++) {
@@ -11,18 +54,32 @@ function enableOperators(){
     elems[i].disabled = false;
     }
 }
-const xmlhttp = new XMLHttpRequest();
 
-function populateCurrencies(){
-    var currencyNames = JSON.parse(this.responseText);
-    for(var i=0; i<currencyNames.length; i++){
-        var item = document.createElement("a");
-        item.setAttribute("class","dropdown-item");
-        item.href = "#"; 
-        var node = document.createTextNode(currencyNames[i]); /*fetching name of the items*/
-        item.appendChild(node);
-        document.getElementsByClassName('dropdown-menu')[0].appendChild(item);
-    }
-    xmlhttp.open("GET", "Currencies.json");
-    xmlhttp.send();
+function populateCurrencies(option, id, path, classname){
+    var item = document.createElement("a");
+    item.setAttribute("class", classname);
+    item.href = "#";
+    item.setAttribute("id", id);
+    var node = document.createTextNode(option);
+    item.appendChild(node);
+    $(path)[0].appendChild(item);
 }
+
+
+$('.currencies').click(function(){
+    selectedCurrency = $(this).attr('id');
+})
+
+$('rates').click(function(){
+    selectedDate = $(this).attr('id');
+    let obj = array.find(o => o.date === selectedDate);
+    if (selectedCurrency === "EUR") {
+        rate = obj.rates.EUR;                
+    } else if (selectedCurrency === "USD") {
+        rate = obj.rates.USD;
+    } else if (selectedCurrency === "GBP") {
+        rate = obj.rates.GBP;
+    }
+    
+    $("#displayRate").val(rate);
+ })
